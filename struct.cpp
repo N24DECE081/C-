b08c96b65676cb1af273dc7275e2033c4c367d4f
N24DECE081 <tmai62682@gmail.com>
2025-10-15 11:42:42 +0700

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -1,119 +1,105 @@
-#pragma once
-#include <iostream>
+#pragma one
+#include<string>
+#include<iostream>
+#define MAXMONHOC 100
+#define MAXLOP 10000
+#define MAXDSCTDT 1000
+
 using namespace std;
 
-
-// Danh sách câu hỏi thi: danh sách liên kết đơn
-struct CauHoi {
-    int ID;                 
-    char NoiDung[256];      
-    char A[100], B[100], C[100], D[100]; 
-    char DapAn;             
+struct MonHoc {//cây nhị phân
+    string MAMH;        // C15
+    string TENMH;
+    struct DanhSachCauHoi *ds_cauhoi; // con trỏ đến danh sách câu hỏi
+    MonHoc *pLeft;
+    MonHoc *pRight;
 };
+typedef MonHoc* TreeMH;
 
-// Nút của danh sách câu hỏi (liên kết đơn)
-struct nodeCH {
-    CauHoi ch;              
-    nodeCH *next;           
+typedef struct DanhSachMonHoc DSMH;
+
+struct CauTrucDeThi
+{
+	string MASV;
+	string MAMH;
+	float DIEM;
+	int sct;
+	int tg;
+	int *bode;
+	int *dapan;
 };
-typedef nodeCH* PTRCH;      
+typedef struct CauTrucDeThi CTDT;
 
-
-// Danh sách môn học: cây nhị phân tìm kiếm cân bằng (AVL)
-struct MonHoc {
-    char MAMH[16];          
-    char TENMH[51];        
-    int height;             
-    PTRCH FirstCH = NULL;   
+struct DanhSachDeThi{
+	CTDT DS_CTDT[MAXDSCTDT];
+	int SLDSDT = 0;
 };
+typedef struct DanhSachDeThi DSDT;
 
-// Nút của cây môn học
-struct nodeMH {
-    MonHoc mh;              
-    nodeMH *left;          
-    nodeMH *right;          
+struct DiemThi{
+	string MAMH;
+	float DIEM;
+	DiemThi *pNext;
 };
-typedef nodeMH* TREE_MH;    
+typedef struct DiemThi NODEDIEMTHI;
 
-
-// Danh sách điểm thi: danh sách liên kết đơn
-struct DiemThi {
-    char MAMH[16];          
-    float diem;             
+struct DanhSachDiem{
+	int SLDiem = 0;
+	NODEDIEMTHI *pHead = NULL;
+	NODEDIEMTHI *pLast = NULL;
 };
+typedef struct DanhSachDiem DSDIEM;
 
-struct nodeDiem {
-    DiemThi dt;             // Dữ liệu điểm thi
-    nodeDiem *next;         
+struct SinhVien{
+	string MASV;
+	string TEN;
+	string HO;
+	string PHAI;
+	string PASSWORD;
+	DSDIEM ds_Diem;
+	SinhVien *pNext;
 };
-typedef nodeDiem* PTRDIEM;  
+typedef struct SinhVien NODESV;
 
-
-struct SinhVien {
-    char MASV[16];          
-    char HO[51];            
-    char TEN[11];           
-    char PHAI[4];           
-    char password[20];      
-    PTRDIEM dsDiemThi = NULL; 
+struct DanhSachSinhVien{
+	int SLSV = 0;
+	NODESV *pHead = NULL;
+	NODESV *pLast = NULL;
 };
+typedef struct DanhSachSinhVien DSSV;
 
-// Nút danh sách liên kết đơn của sinh viên
-struct nodeSV {
-    SinhVien sv;            
-    nodeSV *next;           
+struct Lop{
+	string MALOP;
+	string TENLOP;
+	DSSV ds_SV;
 };
-typedef nodeSV* PTRSV;      
+typedef struct Lop LOP;
 
-// Danh sách lớp: mảng con trỏ (tối đa 10000 lớp)
-const int MAX_LOP = 10000;
-
-struct Lop {
-    char MALOP[16];         
-    char TENLOP[51];        
-    PTRSV FirstSV = NULL;   
+struct DanhSachLop{
+	int SLLOP = 0;
+	LOP *DS_LOP[MAXLOP];
 };
+typedef struct DanhSachLop DSLOP;
 
-
-struct DS_LOP {
-    int n = 0;              
-    Lop* nodes[MAX_LOP];    
+struct CauHoi {//dslk đơn
+    int ID;
+    string NOIDUNG;
+    string A, B, C, D;
+    string DA;
+    CauHoi *pNext;
 };
+typedef struct CauHoi *CauHoiNode;
 
-
-
-// Lưu trữ thông tin 1 bài thi của sinh viên (sau khi thi)
-struct CauTrucDeThi {
-    char MASV[16];         
-    char MAMH[16];          
-    float DIEM;             
-    int soCau;              
-    int thoiGian;           
-    int *boDe = NULL;       // Mảng ID các câu hỏi được chọn (size = soCau)
-    char *dapAn = NULL;     // Mảng đáp án sinh viên chọn (size = soCau)
+struct DanhSachCauHoi {
+    int SLCH = 0;
+    CauHoiNode pHead = NULL;
 };
+typedef struct DanhSachCauHoi DSCH;
 
 
-
-const int MAX_DETHI = 1000;
-struct DS_DETHI {
-    CauTrucDeThi ds[MAX_DETHI]; // Mảng lưu các bài thi
-    int soLuong = 0;            // Số lượng đề thi hiện có
+struct ThoiGian{
+	int Gio;
+	int Phut;
+	int Giay;
 };
-
-
-
-struct ThoiGian {
-    int Gio;
-    int Phut;
-    int Giay;
-};
-
-
-
-int main() {
-    TREE_MH dsMH = NULL;     
-    DS_LOP dsLop;            
-    DS_DETHI dsDeThi;        
-    return 0;
-}
+typedef struct ThoiGian TG;
